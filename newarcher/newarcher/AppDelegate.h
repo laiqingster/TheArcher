3f10466b9cec5d490104bdaa56724d6f55402192
laiqingster <laiqingster@gmail.com>
2013-01-25 16:46:52 +0800

    first init.

---
+++ newarcher/newarcher/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  newarcher
+//
+//  Created by laiqing lai on 13-1-25.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ newarcher/newarcher/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  newarcher
+//
+//  Created by laiqing lai on 13-1-25.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
